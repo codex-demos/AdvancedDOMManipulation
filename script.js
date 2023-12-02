0f92b4308ab709fd520583530a94d0a89f886986
Lewis Benson <tm.lewis.benson@gmail.com>
2023-12-01 20:57:25 -0500

    Create listener for button 1 - creates new li on click Create listener for button 2 - updates text of the li's Create listener for button 3 - removes last item from listener

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const mainContainer = document.getElementById('main-container');
+const listContainer = document.getElementById('list-container');
+const buttons = document.querySelectorAll('button');
+
+mainContainer.style.border = '1px solid black';
+mainContainer.style.padding = '50px';
+
+buttons[0].addEventListener('click', () => {
+  const li = document.createElement('li');
+  li.innerText = 'New Item';
+  listContainer.appendChild(li);
+
+  li.addEventListener('click', () => {
+    li.classList.add('highlight');
+  });
+});
+
+buttons[1].addEventListener('click', () => {
+  const listOfLi = document.querySelectorAll('li');
+  for (let i = 0; i < listOfLi.length; i++) {
+    listOfLi[i].innerText = 'Updated Item';
+  }
+});
+
+buttons[2].addEventListener('click', () => {
+  const listOfLi = document.querySelectorAll('li');
+  const lastItem = listOfLi.length - 1;
+  listOfLi[lastItem].remove();
+});
